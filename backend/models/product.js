ed2a67d72db786cfcc67e34cd971da17149e5364
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-05-15 15:43:30 +0530

    product:details updated

--- backend/models/product.js
+++ backend/models/product.js
@@ -2,11 +2,41 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
     {
-        name : String,
-        taste : String,
-        price : Number,
-        stock : Number,
-        vendorid : String
+        productID:{
+            type: String,
+            required: true,
+            unique: true
+        },
+        name:{
+            type: String,
+            required: true
+        },
+        altNames:[
+            {
+                type: String
+            }
+        ],
+        lablePrice:{
+            type: Number,
+            required: true
+        },
+        price:{
+            type: Number,
+            required: true
+        },
+        stock:{
+            type: Number,
+            required: true
+        },
+        images:[
+            {
+                type: String
+            }
+        ],
+        isAvalaible:{
+            type: Boolean,
+            required: true,
+        }
     }
 );
 const Product = mongoose.model("Products", productSchema);
