f33540006ef2e60c4bf6f1f2e12b5f31e4e9508d
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-05-21 22:51:29 +0530

    order : created order model

---
+++ backend/models/order.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+    {
+        orderID:{
+            type: String,
+            required: true,
+            unique: true
+        },
+        name:{
+            type: String,
+            required: true
+        },
+        email:{
+            type: String,
+            required: true
+        },
+        phone:{
+            type: String,
+            required: true
+        },
+        address:{
+            type: String,
+            required: true
+        },
+        status:{
+            type: String,
+            required: true
+        },
+        products:[
+            {
+                productID: {
+                    type: String,
+                    required: true
+                },
+                name: {
+                    type: String,
+                    required: true
+                },
+                altNames:[
+                    {
+                        type: String
+                    },
+                ]
+            }
+        ],
+        quantity:{
+            type: Number,
+            required: true
+        }
+
+    }
+   
+);
+const Order = mongoose.model("Orders", orderSchema);
+
+export default Order;
